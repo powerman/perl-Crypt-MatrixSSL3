93899a292e85896cc0726e2353caeeedc91ce6df
Alex Efros <powerman@powerman.name>
2016-02-25 10:42:57 +0200

    replace tab with spaces, close #4

--- MatrixSSL3.h
+++ MatrixSSL3.h
@@ -2,12 +2,12 @@
 #define __MATRIXSSL3_H
 
 /* MatrixSSL capabilities */
-#define SHARED_SESSION_CACHE_ENABLED		1
-#define STATELESS_TICKETS_ENABLED		2
-#define DH_PARAMS_ENABLED			4
-#define ALPN_ENABLED				8
-#define SNI_ENABLED				16
-#define OCSP_STAPLES_ENABLED			32
-#define CERTIFICATE_TRANSPARENCY_ENABLED	64
+#define SHARED_SESSION_CACHE_ENABLED            1
+#define STATELESS_TICKETS_ENABLED               2
+#define DH_PARAMS_ENABLED                       4
+#define ALPN_ENABLED                            8
+#define SNI_ENABLED                             16
+#define OCSP_STAPLES_ENABLED                    32
+#define CERTIFICATE_TRANSPARENCY_ENABLED        64
 
 #endif
