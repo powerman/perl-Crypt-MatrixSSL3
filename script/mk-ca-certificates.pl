4a9dc586e3faaf1afa838159bb8879b58a23c1ee
Alex Efros <powerman@powerman.name>
2016-02-26 21:07:24 +0200

    update mk-ca-certificates.pl

--- script/mk-ca-certificates.pl
+++ script/mk-ca-certificates.pl
@@ -2,16 +2,16 @@
 # Update ./ca-certificates.crt using current Firefox CA bundle.
 use warnings;
 use strict;
-use blib;
 use Crypt::MatrixSSL3;
 
-system('perl mk-ca-bundle.pl -u');
+(my $path = $0) =~ s{[^/]*\z}{}ms;
+system("\Q$path\Emk-ca-bundle.pl -u");
 open my $f, '<', 'ca-bundle.crt' or die "open: $!";
 my $bundle = join q{}, <$f>;
 close $f;
 unlink 'ca-bundle.crt';
 
-Crypt::MatrixSSL3::open();
+Crypt::MatrixSSL3::Open();
 
 open $f, '>', 'ca-certificates.crt' or die "open: $!";
 while ($bundle =~ /^(\S[^\n]*)\n=+\n(-----BEGIN CERTIFICATE-----\n.*?\n-----END CERTIFICATE-----\n)/msg) {
@@ -28,4 +28,4 @@ while ($bundle =~ /^(\S[^\n]*)\n=+\n(-----BEGIN CERTIFICATE-----\n.*?\n-----END 
 }
 close $f or die "close: $!";
 
-Crypt::MatrixSSL3::close();
+Crypt::MatrixSSL3::Close();
